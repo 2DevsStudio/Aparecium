d07fff8ad8632b9bc50d9b2bb91273a498bdd636
Im-IgniteDEV <ignitedevpriv@gmail.com>
2022-08-24 18:44:56 +0200

    Custom Command implementation with placeholders and other command execution system

---
+++ src/main/java/com/ignitedev/aparecium/command/custom/CustomCommand.java
@@ -0,0 +1,60 @@
+package com.ignitedev.aparecium.command.custom;
+
+import com.ignitedev.aparecium.interfaces.Sendable;
+import com.ignitedev.aparecium.util.MessageUtility;
+import java.util.List;
+import lombok.Data;
+import net.kyori.adventure.audience.Audience;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @implNote Custom command system for simple plain commands with command execution or text only
+ */
+@Data
+public class CustomCommand  implements Sendable<Audience> {
+
+  @NotNull
+  private final String commandName;
+  @Nullable
+  private final List<String> commandAliases;
+  @Nullable
+  private final String commandPermission;
+
+  /**
+   * @implNote this messages command sender is going to receive on command execution, if message starts with '/' then
+   * it is treated as command, and it will be executed as player, if message starts with '@/' then it will be
+   * executed as console, you can use {PLAYER} placeholder to get executing player nickname
+   */
+  @NotNull
+  private final List<String> commandResponse;
+
+
+  /**
+   * @param audience command executor
+   * @implNote for more information look at {@link #commandResponse comment}
+   */
+  @Override
+  public Audience send(Audience audience) {
+    for (String value : commandResponse) {
+      if(audience instanceof Player player){
+      // replacing placeholders
+        value = value.replace("{PLAYER}", player.getName());
+      }
+      if(value.startsWith("@/")){
+        // performing commands as console
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), value.substring(2));
+      } else if(value.startsWith("/")){
+        // performing commands as player
+        if(audience instanceof Player player){
+          player.performCommand(value.substring(1));
+        }
+      } else {
+        MessageUtility.send(audience, value);
+      }
+    }
+    return audience;
+  }
+}
