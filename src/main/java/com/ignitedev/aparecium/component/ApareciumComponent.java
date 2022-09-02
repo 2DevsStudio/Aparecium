794e14ce5e6e3f75be4c71f5e8a3020cae0c470c
Im-IgniteDEV <ignitedevpriv@gmail.com>
2022-09-02 19:34:30 +0200

    ApareciumComponent; Converting one specified value to another plus #getAsStrings() and #getAsComponents()

--- src/main/java/com/ignitedev/aparecium/component/ApareciumComponent.java
+++ src/main/java/com/ignitedev/aparecium/component/ApareciumComponent.java
@@ -1,41 +1,68 @@
 package com.ignitedev.aparecium.component;
 
-import java.util.ArrayList;
+import com.ignitedev.aparecium.util.text.TextUtility;
 import java.util.List;
 import java.util.function.Supplier;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Singular;
 import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 @Data
 @Builder
 public class ApareciumComponent {
 
-  public interface ListStrings extends Supplier<List<String>> {}
-  public interface ListComponents extends Supplier<List<Component>> {}
+  @NotNull
   @Singular(value = "string")
-  private List<String> strings = new ArrayList<>();
+  private List<String> strings;
 
+  @NotNull
   @Singular(value = "component")
-  private List<Component> components = new ArrayList<>();
+  private List<Component> components;
 
   public ApareciumComponent(ListComponents components) {
     this.components = components.get();
+    this.strings = TextUtility.serializeComponent(components.get());
   }
 
   public ApareciumComponent(ListStrings strings) {
     this.strings = strings.get();
+    this.components = TextUtility.colorizeToComponent(strings.get());
   }
 
-  public ApareciumComponent(String string){
+  public ApareciumComponent(String string) {
     this.strings = List.of(string);
+    this.components = List.of(TextUtility.colorizeToComponent(string));
   }
 
-  public ApareciumComponent(Component component){
+  public ApareciumComponent(Component component) {
     this.components = List.of(component);
+    this.strings = TextUtility.serializeComponent(List.of(component));
   }
+
+  @Nullable
+  public List<String> getAsStrings() {
+    if (!strings.isEmpty()) {
+      return TextUtility.colorize(strings);
+    } else if (!components.isEmpty()) {
+      return TextUtility.serializeComponent(components);
+    }
+    return null;
+  }
+
+  @Nullable
+  public List<Component> getAsComponents() {
+    if (!strings.isEmpty()) {
+      return TextUtility.colorizeToComponent(strings);
+    } else if (!components.isEmpty()) {
+      return components;
+    }
+    return null;
+  }
+
+  public interface ListStrings extends Supplier<List<String>> {}
+
+  public interface ListComponents extends Supplier<List<Component>> {}
 }
-
-
-
