33d6df28fb8fd8b9df3e63230590413b8d4fecba
Im-IgniteDEV <ignitedevpriv@gmail.com>
2022-09-17 02:40:40 +0200

    HologramExample; matching ApareciumComponent

--- src/main/java/com/ignitedev/aparecium/example/HologramExample.java
+++ src/main/java/com/ignitedev/aparecium/example/HologramExample.java
@@ -5,6 +5,7 @@
 package com.ignitedev.aparecium.example;
 
 import com.ignitedev.aparecium.Aparecium;
+import com.ignitedev.aparecium.component.ApareciumComponent;
 import com.ignitedev.aparecium.hologram.SimpleBaseHologram;
 import com.ignitedev.aparecium.hologram.data.HologramEntry;
 import java.util.ArrayList;
@@ -25,7 +26,7 @@ public class HologramExample {
             "testHologram",
             Bukkit.getWorlds().get(0).getSpawnLocation(),
             List.of(
-                new HologramEntry("test First Line", null),
+                new HologramEntry(ApareciumComponent.of("test First Line"), null),
                 new HologramEntry(null, Material.GOLD_BLOCK)),
             new ArrayList<>());
 
@@ -37,7 +38,7 @@ public class HologramExample {
     SimpleBaseHologram secondExampleHologram =
         SimpleBaseHologram.builder()
             .id("testHologram")
-            .line(new HologramEntry("test First Line", null))
+            .line(new HologramEntry(ApareciumComponent.of("test First Line"), null))
             .line(new HologramEntry(null, Material.GOLD_BLOCK))
             .build();
 
