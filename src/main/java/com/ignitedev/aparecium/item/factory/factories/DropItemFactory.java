b731e7e6192e80ae4ca9937240d2460ab4df0b61
Im-IgniteDEV <ignitedevpriv@gmail.com>
2022-09-20 03:26:26 +0200

    Default Factory for DropItem instead of constructors

---
+++ src/main/java/com/ignitedev/aparecium/item/factory/factories/DropItemFactory.java
@@ -0,0 +1,49 @@
+package com.ignitedev.aparecium.item.factory.factories;
+
+import com.ignitedev.aparecium.component.ApareciumComponent;
+import com.ignitedev.aparecium.enums.ItemType;
+import com.ignitedev.aparecium.enums.Rarity;
+import com.ignitedev.aparecium.item.basic.DropItem;
+import java.util.HashMap;
+import java.util.Map;
+import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class DropItemFactory extends DefaultMagicItemFactory<DropItem> {
+
+  public DropItem createItem(
+      @NotNull String id,
+      @NotNull Material material,
+      @Nullable ItemType itemType,
+      @Nullable Rarity rarity,
+      @Nullable ApareciumComponent name,
+      @Nullable ApareciumComponent description,
+      @Nullable Map<String, Object> tags,
+      double dropChance) {
+    return new DropItem(
+        id, material, itemType, rarity, name, description, tags, dropChance, new HashMap<>());
+  }
+
+  public DropItem createItem(
+      @NotNull String id,
+      @NotNull Material material,
+      @Nullable ItemType itemType,
+      @Nullable Rarity rarity,
+      @Nullable ApareciumComponent name,
+      @Nullable ApareciumComponent description,
+      @Nullable Map<String, Object> tags,
+      double dropChance,
+      Map<Material, Double> dropChancesForMaterials) {
+    return new DropItem(
+        id,
+        material,
+        itemType,
+        rarity,
+        name,
+        description,
+        tags,
+        dropChance,
+        dropChancesForMaterials);
+  }
+}
