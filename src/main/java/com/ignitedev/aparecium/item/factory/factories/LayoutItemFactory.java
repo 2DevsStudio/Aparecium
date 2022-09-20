b979cdcba320cbacb4769a2ae6816fe8a9442650
Im-IgniteDEV <ignitedevpriv@gmail.com>
2022-09-20 03:26:20 +0200

    Default Factory for LayoutItem instead of constructors

---
+++ src/main/java/com/ignitedev/aparecium/item/factory/factories/LayoutItemFactory.java
@@ -0,0 +1,26 @@
+package com.ignitedev.aparecium.item.factory.factories;
+
+import com.ignitedev.aparecium.component.ApareciumComponent;
+import com.ignitedev.aparecium.enums.ItemType;
+import com.ignitedev.aparecium.enums.Rarity;
+import com.ignitedev.aparecium.item.basic.LayoutItem;
+import java.util.Map;
+import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class LayoutItemFactory extends DefaultMagicItemFactory<LayoutItem> {
+
+  public LayoutItem createItem(
+      @NotNull String id,
+      @NotNull Material material,
+      @Nullable ItemType itemType,
+      @Nullable Rarity rarity,
+      @Nullable ApareciumComponent name,
+      @Nullable ApareciumComponent description,
+      @Nullable Map<String, Object> tags,
+      double layoutItemInteractionId) {
+    return new LayoutItem(
+        id, material, itemType, rarity, name, description, tags, layoutItemInteractionId);
+  }
+}
