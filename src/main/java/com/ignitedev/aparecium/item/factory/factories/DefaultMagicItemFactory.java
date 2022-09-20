a6f2ac86935f6a1d0f7dd0420eca534b7f1ae4c1
Im-IgniteDEV <ignitedevpriv@gmail.com>
2022-09-20 03:24:57 +0200

    Default Factory for MagicItems instead of constructors

--- src/main/java/com/ignitedev/aparecium/item/factory/factories/DefaultMagicItemFactory.java
+++ src/main/java/com/ignitedev/aparecium/item/factory/factories/DefaultMagicItemFactory.java
@@ -5,25 +5,82 @@
 package com.ignitedev.aparecium.item.factory.factories;
 
 import com.ignitedev.aparecium.Aparecium;
+import com.ignitedev.aparecium.component.ApareciumComponent;
+import com.ignitedev.aparecium.enums.ItemType;
+import com.ignitedev.aparecium.enums.Rarity;
 import com.ignitedev.aparecium.item.MagicItem;
-import com.ignitedev.aparecium.item.factory.RawItemStackFactory;
+import com.ignitedev.aparecium.item.basic.Item;
+import java.util.HashMap;
+import java.util.Map;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @implNote Default MagicItem Factory with implemented text assets
  */
-public class DefaultMagicItemFactory extends RawItemStackFactory {
+public class DefaultMagicItemFactory<T extends Item> extends RawItemStackFactory {
+
+  public T createItem(@NotNull String id, @NotNull Material material) {
+    return createItem(id, material, null, null, null, null, new HashMap<>());
+  }
+
+  public T createItem(
+      @NotNull String id, @NotNull Material material, @Nullable ApareciumComponent name) {
+    return createItem(id, material, null, null, name, null, new HashMap<>());
+  }
+
+  public T createItem(
+      @NotNull String id,
+      @NotNull Material material,
+      @Nullable ItemType itemType,
+      @Nullable Rarity rarity) {
+    return createItem(id, material, itemType, rarity, null, null, new HashMap<>());
+  }
+
+  public T createItem(
+      @NotNull String id,
+      @NotNull Material material,
+      @Nullable ApareciumComponent name,
+      @Nullable ApareciumComponent description) {
+    return createItem(id, material, null, null, name, description, new HashMap<>());
+  }
+
+  public T createItem(
+      @NotNull String id,
+      @NotNull Material material,
+      @Nullable ItemType itemType,
+      @Nullable Rarity rarity,
+      @Nullable ApareciumComponent name,
+      @Nullable ApareciumComponent description) {
+    return createItem(id, material, itemType, rarity, name, description, new HashMap<>());
+  }
+
+  public T createItem(
+      @NotNull String id,
+      @NotNull Material material,
+      @Nullable ItemType itemType,
+      @Nullable Rarity rarity,
+      @Nullable ApareciumComponent name,
+      @Nullable ApareciumComponent description,
+      @Nullable Map<String, Object> tags) {
+    return ((T) new Item(id, material, itemType, rarity, name, description, tags));
+  }
 
   @Override
   public ItemStack buildLore(MagicItem magicItem, ItemStack itemStack) {
     ItemMeta itemMeta = itemStack.getItemMeta();
+    ApareciumComponent description = magicItem.getDescription();
 
-    // PAPER CODE
-    if (Aparecium.isUsingPaper()) {
-      itemMeta.lore(magicItem.getDescription().getAsComponents());
-    } else {  // PAPER CODE END
-      itemMeta.setLore(magicItem.getDescription().getAsStrings());
+    if (description != null) {
+      // PAPER CODE
+      if (Aparecium.isUsingPaper()) {
+        itemMeta.lore(description.getAsComponents());
+      } else { // PAPER CODE END
+        itemMeta.setLore(description.getAsStrings());
+      }
     }
     itemStack.setItemMeta(itemMeta);
     return itemStack;
@@ -32,12 +89,15 @@ public class DefaultMagicItemFactory extends RawItemStackFactory {
   @Override
   public ItemStack buildName(MagicItem magicItem, ItemStack itemStack) {
     ItemMeta itemMeta = itemStack.getItemMeta();
+    ApareciumComponent name = magicItem.getName();
 
-    // PAPER CODE
-    if (Aparecium.isUsingPaper()) {
-      itemMeta.displayName(magicItem.getName().getAsComponent());
-    } else { // PAPER CODE END
-      itemMeta.setDisplayName(magicItem.getName().getAsString());
+    if (name != null) {
+      // PAPER CODE
+      if (Aparecium.isUsingPaper()) {
+        itemMeta.displayName(name.getAsComponent());
+      } else { // PAPER CODE END
+        itemMeta.setDisplayName(name.getAsString());
+      }
     }
     itemStack.setItemMeta(itemMeta);
     return itemStack;
