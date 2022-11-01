aea0a0a9b653cdf7d5d3e0de2f36eff189944a82
Im-IgniteDEV <ignitedevpriv@gmail.com>
2022-11-01 12:55:48 +0100

    Copyrights

--- src/main/java/com/ignitedev/aparecium/util/PageObject.java
+++ src/main/java/com/ignitedev/aparecium/util/PageObject.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022.  Made by 2DevsStudio LLC ( https://2devsstudio.com/ ) using one of our available slaves; IgniteDEV
+ * Copyright (c) 2022. Made by 2DevsStudio LLC ( https://2devsstudio.com/ ), using one of our available slaves: IgniteDEV. All rights reserved.
  */
 
 package com.ignitedev.aparecium.util;
