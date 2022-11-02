5e276f211b90d53bcc72011af44040096c715714
Im-IgniteDEV <ignitedevpriv@gmail.com>
2022-11-02 15:43:30 +0100

    push before journey

---
+++ src/main/java/com/ignitedev/aparecium/config/wrapper/LayerWrapper.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2022. Made by 2DevsStudio LLC ( https://2devsstudio.com/ ), using one of our available slaves: IgniteDEV. All rights reserved.
+ */
+
+package com.ignitedev.aparecium.config.wrapper;
+
+public class LayerWrapper {
+
+}
