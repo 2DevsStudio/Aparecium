ca3519933a52991d2e4c175dad98cb2baae50b6f
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2022-11-08 16:56:23 +0100

    ItemBase Command - Get subcommand

--- src/main/java/com/ignitedev/aparecium/command/ItemBaseCommand.java
+++ src/main/java/com/ignitedev/aparecium/command/ItemBaseCommand.java
@@ -5,6 +5,7 @@ import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
 import co.aikar.commands.annotation.HelpCommand;
+import co.aikar.commands.annotation.Optional;
 import co.aikar.commands.annotation.Subcommand;
 import com.ignitedev.aparecium.Constants;
 import com.ignitedev.aparecium.config.ItemBase;
@@ -13,6 +14,7 @@ import com.ignitedev.aparecium.util.MessageUtility;
 import com.twodevsstudio.simplejsonconfig.interfaces.Autowired;
 import java.util.Map.Entry;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 @CommandAlias("itembase|ib|itemsbase|ibase|itemsb")
 @CommandPermission("aparecium.itembase")
@@ -57,4 +59,11 @@ public class ItemBaseCommand extends BaseCommand {
     MessageUtility.send(player, stringBuilder.toString());
   }
 
+  @Subcommand("get")
+  public void onGet(Player player, String id, @Optional int amount){
+    MagicItem byId = itemBase.getById(id);
+    ItemStack item = byId.toItemStack(amount <= 0 ? 1 : amount);
+
+    player.getInventory().addItem(item);
+  }
 }
