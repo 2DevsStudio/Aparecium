e683426b8935041e1b81b0b38de9d3ab9b8322f4
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2022-11-08 16:57:06 +0100

    ItemType by It purpose

--- src/main/java/com/ignitedev/aparecium/util/ItemUtility.java
+++ src/main/java/com/ignitedev/aparecium/util/ItemUtility.java
@@ -4,6 +4,7 @@
 
 package com.ignitedev.aparecium.util;
 
+import com.ignitedev.aparecium.enums.ItemType;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -74,5 +75,27 @@ public class ItemUtility {
     spawner.setItemMeta(spawnerMeta);
 
     return spawner;
+  }
+
+  public ItemType getItemTypeByMaterial(Material material) {
+    String name = material.name().toLowerCase();
+
+    if (name.contains("sword") || name.contains("bow") || name.contains("crossbow")) {
+      return ItemType.WEAPON;
+    } else if (name.contains("boots")
+        || name.contains("chestplate")
+        || name.contains("leggings")
+        || name.contains("helmet")
+        || name.contains("shield")) {
+      return ItemType.ARMOR;
+    } else if (name.contains("shovel")
+        || name.contains("hoe")
+        || name.contains("axe")
+        || name.contains("pickaxe")) {
+      return ItemType.TOOL;
+    } else if (name.contains("potion")) {
+      return ItemType.POTION;
+    }
+    return ItemType.COMMON;
   }
 }
