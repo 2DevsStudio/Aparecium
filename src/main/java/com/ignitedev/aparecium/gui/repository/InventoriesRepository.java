891ca9de48c2bd7729c7ad49591c08656f1d400d
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2022-11-08 16:57:57 +0100

    Copyrights

--- src/main/java/com/ignitedev/aparecium/gui/repository/InventoriesRepository.java
+++ src/main/java/com/ignitedev/aparecium/gui/repository/InventoriesRepository.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2022. Made by 2DevsStudio LLC ( https://2devsstudio.com/ ), using one of our available slaves: IgniteDEV. All rights reserved.
+ */
+
 package com.ignitedev.aparecium.gui.repository;
 
 import com.ignitedev.aparecium.gui.basic.Layout;
--- src/main/java/com/ignitedev/aparecium/item/basic/LayoutItem.java
+++ src/main/java/com/ignitedev/aparecium/item/basic/LayoutItem.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2022. Made by 2DevsStudio LLC ( https://2devsstudio.com/ ), using one of our available slaves: IgniteDEV. All rights reserved.
+ */
+
 package com.ignitedev.aparecium.item.basic;
 
 import com.ignitedev.aparecium.component.ApareciumComponent;
