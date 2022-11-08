346b382dac8e5a1f1b014530c508266ddc5517ed
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2022-11-08 16:58:02 +0100

    PaperUtility with IncorrectEngineException check method

---
+++ src/main/java/com/ignitedev/aparecium/util/PaperUtility.java
@@ -0,0 +1,15 @@
+package com.ignitedev.aparecium.util;
+
+import com.ignitedev.aparecium.ApareciumMain;
+import com.ignitedev.aparecium.exception.IncorrectEngineException;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PaperUtility {
+
+  public void checkPaper() throws IncorrectEngineException {
+    if (!ApareciumMain.isUsingPaper()) {
+      throw new IncorrectEngineException();
+    }
+  }
+}
