877bdaaa187cf23c6e15271ff333b1d45c5770dd
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2022-11-08 16:56:33 +0100

    CustomCommandsBase additional comments and match CustomCommand constructor

--- src/main/java/com/ignitedev/aparecium/config/CustomCommandsBase.java
+++ src/main/java/com/ignitedev/aparecium/config/CustomCommandsBase.java
@@ -3,6 +3,7 @@ package com.ignitedev.aparecium.config;
 import com.ignitedev.aparecium.command.custom.CustomCommand;
 import com.twodevsstudio.simplejsonconfig.api.Config;
 import com.twodevsstudio.simplejsonconfig.interfaces.Configuration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import lombok.Getter;
@@ -15,19 +16,24 @@ public class CustomCommandsBase extends Config {
 
   private Map<String, CustomCommand> savedCommands = exampleCommands();
 
+  /**
+   * @param commandName name command you are looking for
+   * @return command with given name or null if not found
+   */
   @Nullable
   public CustomCommand getById(String commandName){
     return savedCommands.get(commandName);
   }
 
+  /**
+   * @param alias one of aliases of command you are looking for
+   * @return command with given alias or null if not found
+   */
   @Nullable
   public CustomCommand getByAlias(String alias){
     for (CustomCommand value : savedCommands.values()) {
       List<String> commandAliases = value.getCommandAliases();
 
-      if(commandAliases == null || commandAliases.isEmpty()){
-        continue;
-      }
       if(commandAliases.contains(alias)){
         return value;
       }
@@ -36,7 +42,7 @@ public class CustomCommandsBase extends Config {
   }
 
   private Map<String, CustomCommand> exampleCommands() {
-    return Map.of("aparecium", new CustomCommand("aparecium", null, null,
+    return Map.of("aparecium", new CustomCommand("aparecium", new ArrayList<>(), null,
         List.of("Hey, this is test CustomCommand by Aparecium")));
   }
 
