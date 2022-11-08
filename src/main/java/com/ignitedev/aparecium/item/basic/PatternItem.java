022edcee2faf21bd84d8012ed8b0d4151b1d1d5e
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2022-11-08 16:57:15 +0100

    PatternItem; rollSet and dropChance for Pattern

--- src/main/java/com/ignitedev/aparecium/item/basic/PatternItem.java
+++ src/main/java/com/ignitedev/aparecium/item/basic/PatternItem.java
@@ -1,6 +1,7 @@
 package com.ignitedev.aparecium.item.basic;
 
 import com.ignitedev.aparecium.Aparecium;
+import com.ignitedev.aparecium.util.MathUtility;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
@@ -14,20 +15,25 @@ import org.bukkit.inventory.ItemStack;
 @SuperBuilder(toBuilder = true)
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-public class PatternItem extends DropItem {
+public class PatternItem extends LayoutItem {
+
+  /**
+   * @implNote Global drop chance
+   */
+  private double dropChance;
 
   @Singular("addPattern")
-  private List<DropItem> patterns;
+  private List<PatternItem> patterns;
 
   public PatternItem(MagicItemBuilder<?, ?> builder) {
     super(builder);
   }
 
   /**
-   * @return list of items which pass successful {{{@link #tryLuck()}}}
+   * @return list of items which pass successful {{@link #tryLuck()}}
    */
-  public List<DropItem> roll() {
-    List<DropItem> positiveItems = new ArrayList<>();
+  public List<PatternItem> roll() {
+    List<PatternItem> positiveItems = new ArrayList<>();
 
     this.patterns.forEach(
         patterns -> {
@@ -36,6 +42,33 @@ public class PatternItem extends DropItem {
           }
         });
     return positiveItems;
+  }
+
+  /**
+   * Changes current item to one from patterns
+   */
+  public void rollSet() {
+    for (PatternItem pattern : this.patterns) {
+      PatternItem clone = pattern.clone();
+
+      if (pattern.tryLuck()) {
+        this.setMaterial(clone.getMaterial());
+        this.setId(clone.getId());
+        this.setName(clone.getName());
+        this.setDescription(clone.getDescription());
+        this.setRarity(clone.getRarity());
+        this.setDropChance(clone.getDropChance());
+        this.setTags(clone.getTags());
+        break;
+      }
+    }
+  }
+
+  /**
+   * @return try your luck with {{{@link #dropChance}}}
+   */
+  public boolean tryLuck() {
+    return MathUtility.getRandomPercent(dropChance);
   }
 
   @Override
@@ -50,6 +83,7 @@ public class PatternItem extends DropItem {
   public PatternItem clone() {
     PatternItem clone = ((PatternItem) super.clone());
 
+    clone.dropChance = this.dropChance;
     clone.patterns = this.patterns;
 
     return clone;
