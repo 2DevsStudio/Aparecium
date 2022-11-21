a4af16c2efc544fe2bcc41b9ca34e5fc0f1d96d3
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2022-11-21 19:54:58 +0100

    Moved Paper check Logic to PaperUtility

--- src/main/java/com/ignitedev/aparecium/util/PaperUtility.java
+++ src/main/java/com/ignitedev/aparecium/util/PaperUtility.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2022. Made by 2DevsStudio LLC ( https://2devsstudio.com/ ), using one of our available slaves: IgniteDEV. All rights reserved.
+ */
+
 package com.ignitedev.aparecium.util;
 
 import com.ignitedev.aparecium.engine.ApareciumMain;
@@ -12,4 +16,14 @@ public class PaperUtility {
       throw new IncorrectEngineException();
     }
   }
+
+  public boolean checkPaperClass() {
+    try {
+      Class.forName("com.destroystokyo.paper.ClientOption");
+      return true;
+    } catch (ClassNotFoundException ignored) {
+      return false;
+    }
+  }
+
 }
