975796edc77ddd94bbbb498bbf35fc95b284d231
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2023-01-11 14:28:14 +0100

    Layer Wrapper for byId or direct input

--- src/main/java/com/ignitedev/aparecium/config/wrapper/LayerWrapper.java
+++ src/main/java/com/ignitedev/aparecium/config/wrapper/LayerWrapper.java
@@ -1,7 +1,29 @@
 /*
- * Copyright (c) 2022. Made by 2DevsStudio LLC ( https://2devsstudio.com/ ), using one of our available slaves: IgniteDEV. All rights reserved.
+ * Copyright (c) 2022-2023. Made by 2DevsStudio LLC ( https://2devsstudio.com/ ), using one of our available slaves: IgniteDEV. All rights reserved.
  */
 
 package com.ignitedev.aparecium.config.wrapper;
 
-public class LayerWrapper {}
+import com.ignitedev.aparecium.config.LayerBase;
+import com.ignitedev.aparecium.gui.layer.LayoutLayer;
+import com.twodevsstudio.simplejsonconfig.interfaces.Autowired;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.jetbrains.annotations.Nullable;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class LayerWrapper {
+
+  @Autowired private static LayerBase layerBase;
+
+  @Nullable private String layerId;
+  @Nullable private LayoutLayer layoutLayer;
+
+  @Nullable
+  public LayoutLayer getAvailableLayer() {
+    return this.layoutLayer != null ? this.layoutLayer : layerBase.getById(this.layerId);
+  }
+}
